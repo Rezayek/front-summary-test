60733cae68ea4bb588bd479db6ff044a3bfb329a
Rezayek <yomyfriendhh@gmail.com>
2024-11-30 09:01:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
